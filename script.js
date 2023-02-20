51c7aa8667b3e9646810e620b677fefe7a6368dc
lixuan1128 <ottervelpro@gmail.com>
2023-02-20 17:53:35 +0800

    First commit on lixuan's laptop

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log('~~~~~~~\n /\\_/\\\n( o.o ) < lixuan <3\n > ^ <\n~~~~~~~');
 
-/* navbar active background */
-const home = document.getElementById('navHome');
-const about = document.getElementById('navAbout');
-const blog = document.getElementById('navBlog');
-const contact = document.getElementById('navContact');
+// Navbar Scroll Active
+const home = document.getElementById('nav-home'),
+      about = document.getElementById('nav-about'),
+      blog = document.getElementById('nav-blog'),
+      contact = document.getElementById('nav-contact')
 
-let currentPage = home;
+let currentPage = home
 
 window.addEventListener('scroll', () => {
     const currentScroll = window.pageYOffset
@@ -14,25 +14,32 @@ window.addEventListener('scroll', () => {
     let navs = [home, about, blog, contact]
 
     for (let i = 0;i < navs.length;i++) {
-        if (currentScroll <= maxVH*(i+0.5) && currentScroll >= maxVH*(i-0.5)) {
-			if (navs[i] === currentPage) {
-				return
-			}
-			currentPage = navs[i]
-			currentPage.classList.add("active")
-			
-            navs.splice(i, 1)
-
-            let j = 0
-            do {
-				let handler = navs[j]
-				handler.classList.remove("active")
-				j++
-            } while (j < navs.length)
-
-			return
+        if (currentScroll > maxVH*(i+0.5) || currentScroll < maxVH*(i-0.5)) {
+            continue
         }
+        if (navs[i] === currentPage) {
+            return
+        }
+        currentPage = navs[i]
+        currentPage.classList.add("button-active")
+        
+        navs.splice(i, 1)
+
+        let j = 0
+        do {
+            let handler = navs[j]
+            handler.classList.remove("button-active")
+            j++
+        } while (j < navs.length)
+
+        return
     }
-}, {
-	passive: true
+})
+
+
+const hamburgerbotton = document.getElementsByClassName('hamburger-botton')[0]
+const hamburgerlist = document.getElementsByClassName('hamburger-list')[0]
+
+hamburgerbotton.addEventListener('click', () => {
+    hamburgerlist.classList.toggle('active')
 })
